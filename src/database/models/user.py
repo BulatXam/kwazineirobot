1809fb992f4ac3f9d0778fc8c7ba8a4b0912dbac
bulatxam <Khamdbulat@yandex.ru>
2025-09-14 17:42:18 +0300

    feat: add model

--- src/database/models/user.py
+++ src/database/models/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, BigInteger
+from sqlalchemy import Column, String, BigInteger, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 from .core.base import BaseModel
 from .core.crud import ModelCRUD
@@ -11,3 +12,18 @@ class User(BaseModel, ModelCRUD):
     username = Column(String(100), nullable=True)
     first_name = Column(String(100), nullable=False)
     last_name = Column(String(100), nullable=True)
+
+
+class NeiroResponse(BaseModel, ModelCRUD):
+    __tablename__ = "neiro_response"
+
+    user_id = Column(
+        Integer,
+        ForeignKey("User.id", ondelete='CASCADE'),
+        nullable=False,
+        # no need to add index=True, all FKs have indexes
+    )
+    user = relationship('User', backref='clients')
+
+    msg_type = Column(String, default="text") # text or img
+    prompt = Column(String) # text or img
